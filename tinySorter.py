38e4e3e35f63b92deb8fe1a5c6abe35c3c4d250c
CluelessLynx <astrid_neubauer.1@web.de>
2022-05-02 10:49:41 +0200

    Anzeigen des Rahmens

--- tinySorter.py
+++ tinySorter.py
@@ -1,1 +1,24 @@
 
+import cv2              #Import the OpenCV Library
+import numpy as np      #Import the Numpy library
+import sys
+
+camera = cv2.VideoCapture(0) #create a VideoCapture object with the 'first' camera (your webcam)
+
+while(True):
+    ret, frame = camera.read()             # Capture frame by frame   
+    img = cv2.imread("Bilder/Rahmen.png")
+    img = cv2.resize(img,(1200,800))
+    frame = cv2.resize(frame,(250,200))#Load the image file into memory
+    img[75:275,875:1125]=frame
+    cv2.imshow("Image", img) 
+    
+    if cv2.waitKey(1) & 0xFF == ord(' '):  # Stop if spacebar is detected
+        break
+
+camera.release()                           # Cleanup after spacebar is detected.
+cv2.destroyAllWindows()
+
+
+
+
