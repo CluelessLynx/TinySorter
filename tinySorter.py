0e01462d51a33f8db50ac6343aae31ae688bebb8
CluelessLynx <astrid_neubauer.1@web.de>
2022-05-04 12:12:07 +0200

    Hinzufügen Wecam in Bild

--- tinySorter.py
+++ tinySorter.py
@@ -7,13 +7,13 @@ import sys
 img = cv2.imread("Bilder/Rahmen.png") #
 img = cv2.resize(img, (1200, 800))
 
-camera = cv2.VideoCapture(1)  # create a VideoCapture object with the 'first' camera (your webcam)
+camera = cv2.VideoCapture(0)  # create a VideoCapture object with the 'first' camera (your webcam)
 camera.set(3,320)
 camera.set(4,240)
 camera.set(5,60)
 #camera.set(15, 1.0)
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+#arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 
 
 while (True):
@@ -24,9 +24,8 @@ while (True):
     imgbearbeiten[75:315, 805:1125] = frame # Cam auf Bild kopieren
     cv2.imshow("Image", imgbearbeiten)
 
-    if cv2.waitKey(1) & 0xFF == ord(' '):  # Stop if spacebar is detected
+    if cv2.waitKey(1) & 0xFF == ord(' ') or cv2.waitKey(5) & 0xFF == 27:  # Stop if spacebar is detected
         break
 
 camera.release()  # Cleanup after spacebar is detected.
 cv2.destroyAllWindows()
-
