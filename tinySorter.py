bd15268ede646a63c240c554115698e078caa9be
CluelessLynx <astrid_neubauer.1@web.de>
2022-05-04 11:57:44 +0200

    Hinzufügen Wecam in Bild

--- tinySorter.py
+++ tinySorter.py
@@ -1,24 +1,32 @@
-
-import cv2              #Import the OpenCV Library
-import numpy as np      #Import the Numpy library
+import cv2  # Import the OpenCV Library
+import serial
+import warnings
+import numpy as np  # Import the Numpy library
 import sys
 
-camera = cv2.VideoCapture(0) #create a VideoCapture object with the 'first' camera (your webcam)
+img = cv2.imread("Bilder/Rahmen.png") #
+img = cv2.resize(img, (1200, 800))
 
-while(True):
-    ret, frame = camera.read()             # Capture frame by frame   
-    img = cv2.imread("Bilder/Rahmen.png")
-    img = cv2.resize(img,(1200,800))
-    frame = cv2.resize(frame,(250,200))#Load the image file into memory
-    img[75:275,875:1125]=frame
-    cv2.imshow("Image", img) 
-    
+camera = cv2.VideoCapture(1)  # create a VideoCapture object with the 'first' camera (your webcam)
+camera.set(3,320)
+camera.set(4,240)
+camera.set(5,60)
+#camera.set(15, 1.0)
+
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+
+
+while (True):
+    ret, frame = camera.read()  # Capture frame by frame
+    imgbearbeiten = img     # ausgebe bild zurücksetzen
+    # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    # frame = cv2.resize(frame, (250, 200))
+    imgbearbeiten[75:315, 805:1125] = frame # Cam auf Bild kopieren
+    cv2.imshow("Image", imgbearbeiten)
+
     if cv2.waitKey(1) & 0xFF == ord(' '):  # Stop if spacebar is detected
         break
 
-camera.release()                           # Cleanup after spacebar is detected.
+camera.release()  # Cleanup after spacebar is detected.
 cv2.destroyAllWindows()
-
-
-
 
